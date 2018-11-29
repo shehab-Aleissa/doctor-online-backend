53131db55d84af71ca04fc851b58f9e2dd4e637d
Shehab Ahmed <25254@aum.edu.kw>
2018-11-30 02:28:14 +0300

    ready to test on live server

--- doctor_project/settings.py
+++ doctor_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ahhv$s752ehe4fzcg=-0%6$+=*rasrsrh*e@hz&k7q4je3ad@-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['207.154.246.97', '127.0.0.1', '192.168.1.35']
 
